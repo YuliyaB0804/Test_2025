462ace4487a63768a977dc730b6674f492f2a1c5
YuliyaB0804 <yburak1984@gmail.com>
2025-10-02 18:54:31 +0300

    HW11 changes in directories

--- homeworks/hw11/library/reader.py
+++ homeworks/hw11/library/reader.py
@@ -3,19 +3,22 @@ class Reader:
         self.name = name
 
     def reserve_book(self, book):
-        if book.is_reserved():
+        if book.reserved_by is not None:
             return False
+        # Вызываем метод reserve, который сам проверит, свободна ли книга
         book.reserve(self)
         return True
 
     def cancel_reserve(self, book):
-        result = book.cancel_reserve(self)
-        return bool(result)
+        if book.reserved_by != self:
+            return False
+        book.cancel_reserve(self)
+        return True
 
     def get_book(self, book):
         result = book.get_book(self)
-        return bool(result)
+        return result is None or True
 
     def return_book(self, book):
         result = book.return_book(self)
-        return bool(result)
+        return result is None or True
