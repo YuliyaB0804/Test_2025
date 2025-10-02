0f43f50d3e8862727373a76a6dc7dc376ff0eae2
YuliyaB0804 <yburak1984@gmail.com>
2025-10-02 16:23:20 +0300

    HW11 changes in directories

--- homeworks/hw11/library/book.py
+++ homeworks/hw11/library/book.py
@@ -4,45 +4,37 @@ class Book:
         self.author = author
         self.num_pages = num_pages
         self.isbn = isbn
-        self.is_reserved = False
-        self.is_borrowed = False
         self.reserved_by = None
         self.borrowed_by = None
 
     def reserve(self, reader):
-        if self.is_reserved or self.is_borrowed:
+        if self.reserved_by or self.borrowed_by:
             print(f"Книга '{self.book_name}' уже зарезервирована или выдана")
         else:
-            self.is_reserved = True
             self.reserved_by = reader
             print(f"{reader.name} забронировал книгу '{self.book_name}'")
 
     def cancel_reserve(self, reader):
-        if self.is_reserved and self.reserved_by == reader:
-            self.is_reserved = False
+        if self.reserved_by == reader:
             self.reserved_by = None
             print(f"{reader.name} отменил бронирование книги "
                   f"'{self.book_name}'")
         else:
-            print(f"{reader.name} не может отменить бронирование книги"
-                  f" '{self.book_name}'")
+            print(f"{reader.name} не может отменить бронирование этой книги")
 
     def get_book(self, reader):
-        if self.is_borrowed:
-            print(f"Книгу '{self.book_name}' уже взял другой читатель")
-        elif self.is_reserved and self.reserved_by != reader:
+        if self.borrowed_by:
+            print(f"Книга '{self.book_name}' уже взята другим читателем")
+        elif self.reserved_by and self.reserved_by != reader:
             print(f"Книга '{self.book_name}' зарезервирована другим читателем")
         else:
-            self.is_borrowed = True
             self.borrowed_by = reader
-            if self.is_borrowed and self.borrowed_by == reader:
-                self.is_reserved = False
+            if self.reserved_by == reader:
                 self.reserved_by = None
             print(f"{reader.name} взял книгу '{self.book_name}'")
 
     def return_book(self, reader):
-        if self.is_borrowed and self.borrowed_by == reader:
-            self.is_borrowed = False
+        if self.borrowed_by == reader:
             self.borrowed_by = None
             print(f"{reader.name} вернул книгу '{self.book_name}'")
         else:
