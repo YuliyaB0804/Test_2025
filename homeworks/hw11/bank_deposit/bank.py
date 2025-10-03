f55a5023f93b8d3e1a775722b6cee3f1ce0d184e
YuliyaB0804 <yburak1984@gmail.com>
2025-10-03 12:30:54 +0300

    HW11 changes in directory bank_deposit

--- homeworks/hw11/bank_deposit/bank.py
+++ homeworks/hw11/bank_deposit/bank.py
@@ -17,6 +17,8 @@ class Bank:
     def open_deposit_account(self, client_id, start_balance, years):
         if client_id not in self.clients:
             return False
+        if self.clients[client_id]['deposit'] is not None:
+            return False
         deposit = Deposit(start_balance, years)
         self.clients[client_id]['deposit'] = deposit
         return True
@@ -40,9 +42,16 @@ class Bank:
         if client_id not in self.clients:
             return False
         deposit = self.clients[client_id].get('deposit')
-        if deposit is not None:
-            total_amount = deposit
-            self.clients[client_id]['deposit'] = None
-            return total_amount
-        else:
+        if deposit is None:
             return False
+            # Получаем накопленную сумму
+        interest_rate = 0.10
+        months = deposit.years * 12
+        balance = deposit.start_balance
+        for _ in range(months):
+            interest_month = balance * (interest_rate / 12)
+            balance += interest_month
+        # Удаляем депозит
+        self.clients[client_id]['deposit'] = None
+        total_amount = round(balance, 2)
+        return total_amount
